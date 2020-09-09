85358f06207b35e98871c3d4ada84dc2295c7ec6
JoseLucasapp <josluc2016@gmail.com>
2020-09-09 20:30:58 -0300

    Mini update

--- src/components/secundaryComponents/Organizer.js
+++ src/components/secundaryComponents/Organizer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default ({title, items}) =>{
     return(
         <div>
-            <h2>{title}</h2>
+            <h3>{title}</h3>
             <div>
                 {items.map((item,key)=>(
                     <div key={key}>{item.code}</div>
