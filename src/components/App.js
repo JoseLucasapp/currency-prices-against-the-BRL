3ef66cd50d3e48ff37142eac00be1865ca74890c
JoseLucasapp <josluc2016@gmail.com>
2020-10-22 21:18:17 -0300

    Add loading interface to connect to API

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,12 @@ export default () => {
     useEffect(()=>{
         const loadAll = async()=>{
             let data = await Req.getConverter();
+            document.getElementById('section').style.visibility = 'hidden';
+            if(data){
+                document.getElementById('carregando').style.visibility = 'hidden';
+                document.getElementById('section').style.visibility = 'visible';
+                document.getElementById('carregando').style.position = 'absolute';
+            }
             setCurrency(data);
         }
         loadAll();
@@ -21,12 +27,22 @@ export default () => {
             <div>
                 <Header/>
                 <div>
-                    <section>
+                    <section id='section'>
                         {currency.map((item,key)=>(
                             <div>
                                 <Organizer key={key} title={item.title} items={item.items}/>
                             </div>
                         ))}
+                    </section>
+                    <section id='carregando'>
+                        <div class="spinner">
+                            <div class="rect1"></div>
+                            <div class="rect2"></div>
+                            <div class="rect3"></div>
+                            <div class="rect4"></div>
+                            <div class="rect5"></div>
+                            <p>Carregando</p>
+                        </div>
                     </section>
                 </div>
                 <footer className="footer">
