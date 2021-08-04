14225aff05e486468dbe126d8fc9f24a92bab38b
joselucasapp <josluc2016@gmail.com>
2021-08-04 19:00:16 -0300

    feat: change layout

--- src/components/secundaryComponents/Header.js
+++ src/components/secundaryComponents/Header.js
@@ -5,7 +5,7 @@ import '../styles/Header.css';
 export default () => {
     return(
         <div className="main-header">
-            <h1>Comparação entre o BRL e as principais moedas mundiais</h1>
+            <h1>Comparação entre o real e as principais moedas do mundo</h1>
         </div>
     )
 }
--- src/templates/card.js
+++ src/templates/card.js
@@ -1,14 +1,15 @@
 import React from "react";
 import './styles/card.css';
 
-const Card = ({item})=>{
+const Card = ({item, key})=>{
+    const color = item.varBid[0] === '-'? 'red':'green';
     return(
-        <div className="root">
-            <p><b>Nome:</b> {item.name}</p>
+        <div key={key} className="root">
+            <p><b>Nome:</b> {(item.name).replace('/Real Brasileiro', '')}</p>
             <p><b>Compra:</b> R$ {((item.bid*1).toFixed(2)).replace(".", ",")}</p>
             <p><b>Venda:</b> R$ {(item.ask).replace(".", ",")}</p>
-            <p><b>Variação:</b> R$ {(item.varBid).replace(".", ",")}</p>
-            <p><b>% de Variação:</b> {(item.pctChange*100).toFixed(2)}%</p>
+                <p><b>Variação: </b><span style={{color: color}}>R${((item.varBid*1).toFixed(2)).replace(".", ",")}</span></p>
+            <p><b>% de Variação:</b><span style={{color: color}}> {(item.pctChange*100).toFixed(2)}%</span></p>
             <p><b>Preço maxímo:</b> R$ {(item.high).replace(".", ",")}</p>
             <p><b>Preço mínimo:</b> R$ {(item.low).replace(".", ",")}</p>
         </div>
